9122e0575b8cd6e90259f98a6a3a4bed3bdc480a
Michael Neuhaus <sirtheta@gmail.com>
2021-11-18 13:59:24 +0100

    adds poti with brakout board

--- Interrupts/src/main.cpp
+++ Interrupts/src/main.cpp
@@ -1,23 +1,22 @@
 #include <Arduino.h>
 
-float voltage = 3;
-float frequency;
-
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  frequency = 1 / (1.875 * voltage + 0.8125) * 1000;
+  
 }
 
 void loop()
 {
-  if (voltage < 0.1)
+  float sensorValue = analogRead(A0) * (5.0 / 1023.0);https://github.com/sirtheta/Arduino.git
+  float frequency = 1 / (1.875 * sensorValue + 0.8125) * 1000;
+  if (sensorValue < 0.1)
   {
     digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
     Serial.print("Led is always off!");
   }
-  else if (voltage > 4.9)
+  else if (sensorValue > 4.9)
   {
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.print("Led is always on!");
@@ -29,6 +28,10 @@ void loop()
     delay(frequency);
     digitalWrite(LED_BUILTIN, LOW);
     delay(frequency);
-    Serial.print(frequency);
   }
+    Serial.print("Next: ");
+    Serial.print(frequency);
+    Serial.print(" ");
+    Serial.print("Voltage: ");
+    Serial.print(sensorValue);
 }
