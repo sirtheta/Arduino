2546881e4a1bfded0ab5bd36fd5a0056e436b36f
sirtheta <neo38@bluewin.ch>
2021-11-19 22:59:05 +0100

    implemented using of timer to get te button to switch properly

--- ButtonInterrupt/src/main.cpp
+++ ButtonInterrupt/src/main.cpp
@@ -3,6 +3,7 @@
 const uint8_t BUTTON_PIN = 3;
 void toggle_blinking();
 bool buttonPressed = false;
+int counter = 0;
 
 //Example with reading the volt value from a poti and have an interrupt with a button attached to pin 3
 
@@ -16,16 +17,13 @@ void setup()
 
 void loop()
 {
-  if (buttonPressed)
-  {
-    Serial.println("Button was Pressd!");
-    
     //reading value from poti
-    float sensorValue = analogRead(A0) * (5.0 / 1023.0); 
-    
+    //float sensorValue = analogRead(A0) * (5.0 / 1023.0); 
+    float sensorValue = 1;
     //calculate the value to have 1Hz at 0.1V and 10Hz at 4.9V
     float frequency = 1 / (1.875 * sensorValue + 0.8125) * 1000; 
-    
+  if (buttonPressed)
+  {   
     //Turn LED off if value is below 0.1V
     if (sensorValue < 0.1)
     {
@@ -47,17 +45,26 @@ void loop()
       delay(frequency);
       digitalWrite(LED_BUILTIN, LOW);
       delay(frequency);
-    }
       Serial.print("Frequency: ");
       Serial.print(frequency);
       Serial.println(" ");
       Serial.print("Voltage: ");
       Serial.print(sensorValue);
+      Serial.println("Counter");
+      Serial.print(counter);
     }
   }
+  else
+  {
+    Serial.println("Counter");
+    Serial.print(counter);
+    delay(2000);
+  }
+}
 
 //this method gets called if one presses the button. See setup--> attachInterrupt
 void toggle_blinking()
 {
   buttonPressed = !buttonPressed;
+  counter += 1;
 }
--- TimerInterrupt/src/main.cpp
+++ TimerInterrupt/src/main.cpp
@@ -1,65 +1,100 @@
 #include <Arduino.h>
 #include "NRF52_MBED_TimerInterrupt.h"
-#include "defines.h"
+#define TIMER_INTERVAL_MS 30
+#define SENSOR_VALUE 4.89
+
+void toggle_blinking();
+void timerHandler(void); 
+void blink();
+void getSensorValue();
+void debugRoutine();
 
 const uint8_t BUTTON_PIN = 3;
-void toggle_blinking();
 bool buttonPressed = false;
-
+bool bBlink = false;
+int counter = 0;
+float sensorValue;
+float frequency;
 
 // Init NRF52 timer NRF_TIMER3
 NRF52_MBED_Timer ITimer(NRF_TIMER_3);
-
-
-void TimerHandler(void)
-{
-}
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggle_blinking, FALLING);
-  ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler);
-
 }
 
 void loop()
 {
-  if (buttonPressed)
+  getSensorValue();
+  if (bBlink)
   {
-    Serial.println("Button was Pressd!");
-    
-    float sensorValue = analogRead(A0) * (5.0 / 1023.0);
-    float frequency = 1 / (1.875 * sensorValue + 0.8125) * 1000;
-    if (sensorValue < 0.1)
-    {
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(1000);
-      Serial.print("Led is always off!");
-    }
-    else if (sensorValue > 4.9)
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.print("Led is always on!");
-      delay(1000);
-    }
-    else
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(frequency);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(frequency);
-    }
-      Serial.print("Frequency: ");
-      Serial.print(frequency);
-      Serial.println(" ");
-      Serial.print("Voltage: ");
-      Serial.print(sensorValue);
-    }
+    blink();
   }
+}
 
+void getSensorValue()
+{
+  sensorValue = SENSOR_VALUE;
+  frequency = 1 / (1.875 * sensorValue + 0.8125) * 1000;
+}
+  
+void blink()
+{
+  if (sensorValue < 0.1)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    Serial.print("Led is always off!");
+  }
+  else if (sensorValue > 4.9)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print("Led is always on!");
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(frequency);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(frequency);
+  }
+  //debugRoutine();
+}
+
+void debugRoutine()
+{
+  Serial.print("Frequency: ");
+  Serial.print(frequency);
+  Serial.println(" ");
+  Serial.print("SensorValue: ");
+  Serial.print(sensorValue);
+  Serial.println(" ");
+  Serial.println("counter: ");
+  Serial.println(counter);
+}
+
+//this method gets called if one presses the button. See setup--> attachInterrupt
 void toggle_blinking()
 {
-  buttonPressed = !buttonPressed;
+  buttonPressed = true;
+  ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, timerHandler);
+}
+
+void timerHandler(void)
+{  
+  if (buttonPressed)
+  {
+    counter += 1;
+  }    
+  if (counter > 5)
+  {
+    counter = 0;
+    buttonPressed = false;
+    bBlink = !bBlink;
+    ITimer.detachInterrupt();
+  }    
 }
